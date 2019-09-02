ea5a8b26800ab418bc28525a35ea869bd4b05715
Henrique <hiquesc_@hotmail.com>
2019-09-02 20:20:46 -0300

    cabeçalho e navegação

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
 	<v-app>
+		<Header />
 		<v-content>
 			<v-container>
 				<router-view>
-					
+
 				</router-view>
 			</v-container>
 		</v-content>
@@ -11,8 +12,10 @@
 </template>
 
 <script>
-export default {
+import Header from './components/Header'
 
+export default {
+	components: { Header }
 }
 </script>
 
